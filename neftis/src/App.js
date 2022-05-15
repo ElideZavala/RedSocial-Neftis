8130ef30a505b076ebb66e508b049374f69b66c4
ElideZavala <zavalavinagreelide@gmail.com>
2022-05-15 12:37:07 -0500

    solve

---
+++ neftis/src/App.js
@@ -0,0 +1,34 @@
+import "./styles/App.scss"
+
+import "./index.css"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Post from './pages/Post';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <div className="App light">
+      <Router>
+        <Switch>
+          {/* // Nombre de la ruta se llamada register. */}
+          <Route exact path="/register"> 
+          {/* // Nuestra pagina register. */}
+            <Register />
+          </Route>
+          <Route exact path="/">
+            <Login /> 
+          </Route>
+          <Route exact path='/post/:id'>
+            <Post/>
+          </Route>
+          {/* Pagina en cado de no encontrarse ninguna.  */}
+          <Route> <NotFound/> </Route> 
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ neftis/src/pages/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const Login = () => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+  return (
+    <div className="login "> {/* Colocar el thema Dark */}
+		<div className="login__theme"></div>
+      <h3 className="login__header">Social <span>Neftis</span></h3>
+		<h6 className="login__subheader">Login</h6>
+		<div className="login__data">
+			<form className="login__data--form">
+				<input 
+					className="login__data--form__email"
+					type="email"
+					value={email}
+					onChange={(e)=> setEmail(e.target.email)} 
+					placeholder="Type your email">
+				</input>
+				<input 
+					className="login__data--form__password"
+					type="password"
+					onChange={(e)=> setPassword(e.target.password)} 
+					placeholder="Type your password">
+				</input>
+				<button className="login__data--form__button" type="submit">Log In</button>
+			</form>
+		</div>
+    </div>
+  )
+}
+
+export default Login
