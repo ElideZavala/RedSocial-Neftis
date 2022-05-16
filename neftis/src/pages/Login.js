a1e0a18f840b349b9f1f53e4c94735049beb65da
ElideZavala <zavalavinagreelide@gmail.com>
2022-05-16 12:36:19 -0500

    solve err in type

--- neftis/src/pages/Login.js
+++ neftis/src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Login = () => {
 	const [email, setEmail] = useState('')
@@ -22,12 +24,12 @@ const Login = () => {
 				</input>
 				<input 
 					className="login__data--form__password"
-					type="password"
-					value={showpass ? "type" : "password"} /*{Si showpass es true sera type si es false password }*/
+					type={showpass ? "type" : "password"} /*{Si showpass es true sera type si es false password }*/
+					value={password}
 					onChange={(e)=> setPassword(e.target.password)} 
 					placeholder="Type your password">
 				</input>
-				<small className="login__data--form__showpass" onClick={()=>setShowass(!showpass)}> show </small> 
+				<small className="login__data--form__showpass" onClick={()=>setShowass(!showpass)}>{showpass ? "Hide" : "Show" }</small>
 				<button className="login__data--form__button" type="submit">Log In</button>
 				<p onClick={()=>setShowass(!showpass)} className="login__data--form__small">Do not have account <Link to="register">Create Here</Link></p>
 			</form>
