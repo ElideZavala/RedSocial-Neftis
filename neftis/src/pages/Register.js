cbe0590fbd33e2640cbf03627eb8ccace9dcc079
ElideZavala <zavalavinagreelide@gmail.com>
2022-05-20 16:25:03 -0500

    register values

--- neftis/src/pages/Register.js
+++ neftis/src/pages/Register.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux'
 
 const Register = () => {
 	const initialState = {username:'', fullname:'', email:'', password:'', confirmPassword:'', gender:'male'}
@@ -7,11 +8,26 @@ const Register = () => {
 	const [showpass, setShowpass] = useState(false)
 	const [showconfpass, setShowconfpass] = useState(false)
 	const [userData, setuserData] = useState(initialState)
+	const {username, fullname, email, password, confirmPassword, gender} = userData;
+
+	const { auth } = useSelector(state => state);
+	const dispatch = useDispatch();
+	const history = useHistory(); // Mandar al Usuario a una ruta de inicio.
 
 	const handleChange = (e) => {
 		const {name, value} = e.target;
 		setuserData({...userData, [name]:value})
 	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	}
+
+	// useEffect(() => {
+	// 	if(auth.token) {
+	// 		history.push('/')
+	// 	}
+	// },[auth.token, history])
 
   return (
 	<div className="register "> {/* Colocar el thema Dark */}
@@ -19,50 +35,66 @@ const Register = () => {
 	<h3 className="register__header">Social <span>Neftis</span></h3>
 	<h6 className="register__subheader">Register</h6>
 	<div className="register__data">
-		<form className="register__data--form">
+		<form className="register__data--form" onSubmit={handleSubmit}>
 			<input 
 				className="register__data--form__fullname"
 				type="text"
 				value={fullname}
-				onChange={(e)=> setFullname(e.target.fullname)} 
+				name="fullname"
+				onChange={handleChange} 
 				placeholder="Type your fullname">
 			</input>
+
 			<input 
 				className="register__data--form__username"
 				type="text"
-				value={username}
-				onChange={(e)=> setUsername(e.target.username)} 
+				name="username"
+				value={username.toLowerCase().replace(/ /g,'')} /* Union de las palabras*/
+				onChange={handleChange} 
 				placeholder="Type your Username">
 			</input>
+
 			<input 
 				className="register__data--form__email"
 				type="email"
 				value={email}
-				onChange={(e)=> setEmail(e.target.email)} 
+				name="email"
+				onChange={handleChange}
 				placeholder="Type your Email">
 			</input>
+
 			<input 
 				className="register__data--form__password"
 				type={showpass ? "type" : "password"}
 				value={password}
-				onChange={(e)=> setPassword(e.target.value)} 
+				name="password"
+				onChange={handleChange} 
 				placeholder="Type your password">
 			</input>
 			<small className="register__data--form__showRegPass" onClick={()=>setShowpass(!showpass)}>{showpass ? "Hide" : "Show" }</small>
+
 			<input 
 				className="register__data--form__confirmPassword"
 				type={showconfpass ? "type" : "password"}
 				value={confirmPassword}
-				onChange={(e)=> setConfirmPassword(e.target.value)} 
+				name="confirmPassword"
+				onChange={handleChange}
 				placeholder="Confirm your Password">
 			</input>
-			<select className="register__data--form__select" value={gender} onChange={(e) => setGender(e.target.value)} placeholder="Gender">
+
+			<select 
+				className="register__data--form__select" 
+				value={gender} 
+				onChange={handleChange}
+			 	placeholder="Gender">
 				<option value="">Gender:</option>
 				<option value="male">Male</option>
 				<option value="female">Female</option>
 				<option value="other">Other</option>
 			</select>
+
 			<small className="register__data--form__showRegConfPass" onClick={()=>setShowconfpass(!showconfpass)}>{showconfpass ? "Hide" : "Show" }</small>
+
 			<button className="register__data--form__button" type="submit">Sing Up</button>
 			<small  className="register__data--form__small">Already have an account <Link to="/">Log In Here</Link></small>
 		</form>
